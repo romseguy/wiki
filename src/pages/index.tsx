11f386344bd4162d1e5c74be5b0d44a15fb91c6f
romseguy <contact@romseguy.com>
2026-07-29 12:29:38 +0200

    2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 //@ts-nocheck
-import { ChatIcon } from "@chakra-ui/icons";
-import { Checkbox, HStack, VStack } from "@chakra-ui/react";
+import { ChatIcon, ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { Button, Checkbox, HStack, Spinner, VStack } from "@chakra-ui/react";
 import { useGetTopicsQuery } from "features/api";
 import { Layout } from "features/layout";
 import { PageProps } from "main";
 import { useState } from "react";
+import { FaRulerHorizontal } from "react-icons/fa";
 import { wrapper } from "store";
 
 const IndexPage = ({ ...props }: PageProps) => {
@@ -22,7 +23,10 @@ const IndexPage = ({ ...props }: PageProps) => {
   // console.log("🚀 ~ IndexPage ~ topics:", topics);
 
   const topicsQuery = useGetTopicsQuery();
-  const topics = topicsQuery.data || [];
+  const topics =
+    [...(topicsQuery.data || [])].sort((a, b) =>
+      a.createdAt < b.createdAt ? 1 : -1,
+    ) || [];
 
   return (
     <Layout {...props}>
@@ -31,29 +35,70 @@ const IndexPage = ({ ...props }: PageProps) => {
           <label>Afficher les messages ?</label>
           <Checkbox onChange={() => setIsMessagesVisible(!isMessagesVisible)} />
         </HStack>
-        {topics.map((topic) => (
-          <VStack key={topic._id}>
-            <HStack>
-              <ChatIcon />{" "}
-              <a
-                href={`https://data.romseguy.com/${topic.org}/d/${topic._id}`}
-                target="_blank"
-              >
-                {topic.topicName}
-              </a>
-            </HStack>
-            {isMessagesVisible &&
-              topic.topicMessages.map((m) => (
-                <VStack key={m._id}>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: m.message.replaceAll("http", "xxxx"),
-                    }}
-                  />
-                </VStack>
-              ))}
-          </VStack>
-        ))}
+
+        {topicsQuery.isLoading && <Spinner />}
+
+        {!topicsQuery.isLoading && (
+          <>
+            {topics.map((topic, index) => (
+              <VStack key={topic._id} width="100%" overflowX="clip">
+                <a
+                  name={"t-" + index}
+                  href={`https://data.romseguy.com/${
+                    topic.org || topic.event
+                  }/d/${topic._id}`}
+                  target="_blank"
+                >
+                  <Button>
+                    <ChatIcon mr={1} /> {topic.topicName}{" "}
+                  </Button>
+                </a>
+
+                <HStack>
+                  {index > 0 && (
+                    <a href={"#t-" + Number(index - 1)}>
+                      <Button>
+                        <ChevronLeftIcon />
+                      </Button>
+                    </a>
+                  )}
+                  {index < topics.length - 1 && (
+                    <a href={"#t-" + Number(index + 1)}>
+                      <Button>
+                        <ChevronRightIcon />
+                      </Button>
+                    </a>
+                  )}
+                </HStack>
+
+                {isMessagesVisible &&
+                  topic.topicMessages.map((m, index) => {
+                    let msg = m.message.replaceAll("http", "xxxx");
+                    msg = msg.replaceAll(
+                      "<img",
+                      "<img style='max-width: 200px;'",
+                    );
+
+                    return (
+                      <VStack key={m._id} minWidth="100%">
+                        <div
+                          style={{ minWidth: "100%" }}
+                          dangerouslySetInnerHTML={{
+                            __html: msg,
+                          }}
+                        />
+                        {index !== topic.topicMessages.length - 1 && (
+                          <hr
+                            style={{ border: "1px solid black", width: "100%" }}
+                          />
+                        )}
+                      </VStack>
+                    );
+                  })}
+              </VStack>
+            ))}
+          </>
+        )}
       </VStack>
     </Layout>
   );
